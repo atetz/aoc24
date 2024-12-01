46cbb4444b830dc1fd556433774526dd96efaf02
Adam Tetz <mail@adamtetz.nl>
2024-12-01 19:41:37 +0100

    polishing part 1 and two.

--- src/day1/day1.py
+++ src/day1/day1.py
@@ -14,34 +14,26 @@ def create_location_lists(cleaned_input: str) -> List[List]:
     right_locations = []
 
     for locations in cleaned_input:
-        seperator = "-"
-        left_locations.append(int(locations[: locations.index(seperator)]))
-        right_locations.append(int(locations[locations.index(seperator) + 1 :]))
+        left, right = map(int, locations.split("-"))
+        left_locations.append(left)
+        right_locations.append(right)
 
-        left_locations.sort()
-        right_locations.sort()
+    left_locations.sort()
+    right_locations.sort()
     return left_locations, right_locations
 
 
 def solve_part_one(left_locations: List, right_locations: List) -> int:
-    total_distance = 0
-    for i in range(len(left_locations)):
-        left_location = left_locations[i]
-        right_location = right_locations[i]
-        if left_location > right_location:
-            total_distance += left_location - right_location
-        else:
-            total_distance += right_location - left_location
+    total_distance = sum(
+        abs(left - right) for left, right in zip(left_locations, right_locations)
+    )
     print(f"Part one: {total_distance}")
     return total_distance
 
 
 def solve_part_two(left_locations: List, right_locations: List) -> int:
-    similarity_score = 0
     counts = Counter(right_locations)
-
-    for location in left_locations:
-        similarity_score += location * counts[location]
+    similarity_score = sum(location * counts[location] for location in left_locations)
 
     print(f"Part two {similarity_score}")
     return similarity_score
