afb36bfa138e52878a005248912d14f323d3a368
Adam Tetz <mail@adamtetz.nl>
2024-12-12 17:51:52 +0100

    day 11 with caching.

--- src/day11/day11.py
+++ src/day11/day11.py
@@ -1,14 +1,9 @@
+from typing import Dict, List, Tuple
+
 from utils.helpers import read_puzzle_input_as_string
 
 
-def main():
-    data = read_puzzle_input_as_string(
-        11,
-    )
-
-    stones = [int(stone) for stone in data.split(" ")]
-    print(stones)
-    blinks = 25
+def bruteforce_count(stones: List[int], blinks: int):
     for blink in range(blinks):
         output = []
         for stone in stones:
@@ -22,8 +17,44 @@ def main():
             else:
                 output.append(stone * 2024)
         stones = output
+    return stones
 
-    print(len(stones))
+
+def cached_count(
+    cache: Dict[Tuple[int, int], int], stone: List[int], blinks: int
+) -> int:
+    if (stone, blinks) in cache:
+        return cache[(stone, blinks)]
+    if blinks == 0:
+        return 1
+    if stone == 0:
+        result = cached_count(cache, 1, blinks - 1)
+        cache[(stone, blinks)] = result
+        return result
+    length = len(str(stone))
+    if length % 2 == 0:
+        result = cached_count(
+            cache, int(str(stone)[: length // 2]), blinks - 1
+        ) + cached_count(cache, int(str(stone)[length // 2 :]), blinks - 1)
+        cache[(stone, blinks)] = result
+        return result
+
+    result = cached_count(cache, stone * 2024, blinks - 1)
+    cache[(stone, blinks)] = result
+    return result
+
+
+def count_stones(stones: List[int], blinks: int) -> int:
+    cache = {}
+    return sum(cached_count(cache, stone, blinks) for stone in stones)
+
+
+def main():
+    data = read_puzzle_input_as_string(11)
+    stones = [int(stone) for stone in data.split(" ")]
+
+    print(f"Part one: {count_stones(stones, 25)}")
+    print(f"Part two: {count_stones(stones, 75)}")
 
 
 if __name__ == "__main__":
